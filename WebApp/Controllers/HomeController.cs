31a754bf3abfee2191eb74570ab9175fbe52efbd
pkellner <peter@peterkellner.net>
2017-09-29 22:16:39 -0700

    first checkin react redux wiht node

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.NodeServices;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            ViewData["RequestId"] = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            return View();
+        }
+
+        public async Task<IActionResult> About([FromServices] INodeServices nodeServices)
+        {
+
+            ViewData["ResultFromNode"] = await nodeServices.InvokeAsync<string>("NodeSrc/myNodeModule.js");
+            return View(viewName: "About");
+
+        }
+    }
+}
